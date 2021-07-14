844f35f7033a0fa095288d702b8e580940a86839
Arayik Hambardzumyan <arayik.hambardzumyan@org.fcc.am>
2021-07-14 21:04:29 +0400

    added feature

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let snake = [46, 45, 44];
 let snakeBoard = document.querySelector('.snake-board');
-let direction = 1; //direction 1 = left, direction -1 = right, direction 10 = up, direction -10 = down
+let snakeDirection = 1; //direction 1 = left, direction -1 = right, direction 10 = up, direction -10 = down
 let previousDirection = 1;
 let boardCells = snakeBoard.children;
 let snakeMovement;
+let tail;
 
 //==== Options =====
 let width = 10;
@@ -18,26 +19,29 @@ initGame();
 function initGame() {
     if (boardCells.length === 0) {
         createBoard();
-        document.addEventListener("keydown", (event) => changeSnakeDirection(event.key));
+        document.addEventListener("keydown", (event) => addSnakeDirectionKeybinds(event.key));
     };
     clearInterval(snakeMovement);
     generateSnakeFood();
     setInitialSnakePosition();
-    snakeMovement = setInterval(() => moveSnake(direction), speed * 1000)
+    snakeMovement = setInterval(() => moveSnake(snakeDirection), speed * 1000)
 }
 
 function moveSnake() {
+
     if (hasSnakeCrashed()) {
-        // alert("Game Over!");
+        alert("Game Over!");
         clearInterval(snakeMovement);
         return;
     };
-    if (boardCells[snake[0] + direction].classList.contains('snake')) {
-        direction = -previousDirection;
-    }
-    let tail = snake.pop();
+    if (boardCells[snake[0] + snakeDirection].classList.contains('snake')) {
+        snakeDirection = -previousDirection;
+    };
+
+    tail = snake.pop();
     boardCells[tail].classList.remove('snake');
-    let snakeHead = snake[0] + direction;
+
+    let snakeHead = snake[0] + snakeDirection;
     snake.unshift(snakeHead);
     boardCells[snakeHead].classList.add('snake');
     hasSnakeEaten();
@@ -48,9 +52,8 @@ function createBoard() {
         let cell = document.createElement('div');
         cell.classList.add('board-cell');
         snakeBoard.appendChild(cell);
-    }
-
-}
+    };
+};
 
 function setInitialSnakePosition() {
     snake.forEach(part => {
@@ -58,38 +61,49 @@ function setInitialSnakePosition() {
     });
 };
 
-function changeSnakeDirection(arrowDirection) {
+function addSnakeDirectionKeybinds(arrowDirection) {
     switch (arrowDirection) {
         case "ArrowDown":
-            direction = 10;
-            previousDirection = 10;
+            setSnakeDirection(10);
             break;
         case "ArrowUp":
-            direction = -10;
-            previousDirection = -10;
+            setSnakeDirection(-10);
             break;
         case "ArrowLeft":
-            direction = -1;
-            previousDirection = -1;
+            setSnakeDirection(-1);
             break;
         case "ArrowRight":
-            direction = 1;
-            previousDirection = 1;
+            setSnakeDirection(1);
             break;
     };
+};
+
+function setSnakeDirection(direction) {
+    snakeDirection = direction;
+    previousDirection = direction;
 }
 
 function hasSnakeCrashed() {
-    if (snake[0] + direction >= boardSize ||
-        snake[0] % width === width - 1 && direction === 1 ||
-        snake[0] % width === 0 && direction === -1 ||
-        snake[0] - width < 0 && direction === -10) {
+    if (snake[0] + snakeDirection >= boardSize ||
+        snake[0] % width === width - 1 && snakeDirection === 1 ||
+        snake[0] % width === 0 && snakeDirection === -1 ||
+        snake[0] - width < 0 && snakeDirection === -10 ||
+        boardCells[snake[0] + snakeDirection].classList.contains('snake') && snakeDirection === -previousDirection
+    ) {
         return true;
     } else {
         return false;
-    }
-}
+    };
+};
 
+function hasSnakeEaten() {
+    if (boardCells[snake[0]].classList.contains('food')) {
+        boardCells[snake[0]].classList.remove('food');
+        snake.push(tail);
+        boardCells[tail].classList.add('snake')
+        generateSnakeFood();
+    };
+};
 
 function generateSnakeFood() {
     let foodIndex;
@@ -97,14 +111,5 @@ function generateSnakeFood() {
         foodIndex = Math.floor(Math.random() * boardSize);
     } while (snake.includes(foodIndex));
     boardCells[foodIndex].classList.add('food');
-}
+};
 
-function hasSnakeEaten() {
-    if (boardCells[snake[0]].classList.contains('food')) {
-        boardCells[snake[0]].classList.remove('food');
-        generateSnakeFood();
-    }
-}
-
-
-// function
